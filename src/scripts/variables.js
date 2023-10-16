4d796dce05ef2f4525f0f299b1c5b6290499dcb0
DawidRaczek <dawid.raczek6666@gmail.com>
2023-10-16 12:22:16 +0200

    desription for variables

---
+++ src/scripts/variables.js
@@ -0,0 +1,71 @@
+// Redeklaracja zmiennych
+// Deklaracja zmiennej do stworzenia jej za pomocą np. let x;
+//
+//
+// var x1 = 42;
+// var x1 = 666;
+//
+// let x2 = 42;
+// let x2 = 666; // nie można redeklarować, błąd
+//
+// const x3 = 42;
+// const x3 = 666; // nie można redeklarować, błąd
+
+//======================================================================
+
+// Reinicjalizacja
+// Przypisanie nowej wartości do istniejącej zmiennej np. x = 42;
+//
+// var x1 = 42;
+// x1 = 666;
+//
+// let x2 = 42;
+// x2 = 666;
+//
+// const x3 = 42;
+// x3 = 666; // const nie można reinicjalizować
+
+//===================================================================================
+
+// Hoisting
+// Przenoszenie deklaracji zmiennych, deklaracja funkcji nazwanych, deklaracji klas na
+// góre aktualnie przetwarzanego zasięgu. TDZ - temporary dead zone (tymczasowa strefa śmierci)
+
+//=================================================================================
+
+// Scope - zasięg
+// Widoczność, dostępność identyfikatorów (nazwy zmiennych, funkcji i klas)
+// - global - wszędzie jest coś dostępne
+// - local - dostępne tylko w aktualnym zasięgu
+// - function scope - tworzony przez ciało funkcji
+// - block scope - tworzony przez block, dowolna para 2 klamerek
+
+function doSmth (){
+    var x1 = 42;
+    let x2 = 42;
+    const x3 = 42;
+}
+
+doSmth();
+// console.log(x1, x2, x3)  są niedostępne w aktualnym scope, bo są zadeklarowane wewnątrz funkcji
+
+// if (true) {
+//     var x1 = 42;
+//     let x2 = 42;
+//     const x3 = 42;
+// }
+// console.log(x1);
+// console.log(x1, x2, x3)
+
+
+// let x = 42;
+
+// function doSmthMagic (){
+//     x = 666;  // kiedy nie ma deklaracji zmiennej to szuka w zasięgu otaczającym / wyżej
+//     // let x = 666;  // kiedy jest deklaracja, to jest tworzona nowa zmienna, a ta wyżej nie jest używana
+//     console.log(x);
+// }
+//
+// doSmthMagic();
+// console.log(x);
+
