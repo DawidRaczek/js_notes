2fc0b0bbe2e2368d467c8059503f13a58e4a36fc
DawidRaczek <dawid.raczek6666@gmail.com>
2023-10-24 11:15:04 +0200

    update

--- src/scripts/spreadVSrest.js
+++ src/scripts/spreadVSrest.js
@@ -6,3 +6,11 @@ const x = [1, 2, 3, 4];
 // [...x] => [1,2,3,4] (shallow copy)
 // [...x, ...x] => [1,2,3,4,1,2,3,4]
 // [x,x] => [[1,2,3,4], [1,2,3,4]]
+
+// rest operator with destructuring
+
+const [a, b, ...rest] = x; // a = 1, b = 2, rest = [3, 4]
+
+function name(a, b, ...digits) {} // a = 1, b = 2, digits = [3, 4, 5, 6]
+
+name(1, 2, 3, 4, 5, 6)
